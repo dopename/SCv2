4313647b7ddb1f159451c7cafc7b88a021555f6b
nmarini <marini.nick@gmail.com>
2018-07-12 09:55:37 -0700

    First Redux commit

--- frontend/src/reducers/discovery.js
+++ frontend/src/reducers/discovery.js
@@ -11,7 +11,7 @@ export default function discovery(state = initialState, action) {
 		case "FETCH_ALL_INDUSTRY":
 			let returnData = helpers.fetchListAPICall("industry");
 			let newState = state;
-			newState.allIndustry = returnData;
+			newState.allIndustry = [...newState.allIndustry, ...returnData]
 			return newState
 		default:
 			return state;
