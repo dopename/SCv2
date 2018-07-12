4f3b39f7d7e375e39e491b6409bad36fb0b9818f
nmarini <marini.nick@gmail.com>
2018-07-12 13:37:40 -0700

    First Redux commit

--- frontend/src/reducers/discovery.js
+++ frontend/src/reducers/discovery.js
@@ -13,10 +13,10 @@ export default function discovery(state = initialState, action) {
 			return {...state, industries: action.industries, selectedIndustries: action.industries.map(e => e.pk)}
 		case "CHECK_BOX":
 			if (state.selectedIndustries.indexOf(action.pk) > -1) {
-				return {...state, selectedIndustries: selectedIndustries.splice(state.selectedIndustries.indexOf(action.pk), 1)}
+				return {...state, selectedIndustries: state.selectedIndustries.splice(state.selectedIndustries.indexOf(action.pk), 1)}
 			}
 			else {
-				return {...state, selectedIndustries: [...selectedIndustries, action.pk] }
+				return {...state, selectedIndustries: [...state.selectedIndustries, action.pk] }
 			}
 		default:
 			return state;
