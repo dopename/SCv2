443527ce034d37837858b605661c12fda3011570
nmarini <marini.nick@gmail.com>
2018-07-12 11:50:21 -0700

    First Redux commit

--- frontend/src/components/Discovery.jsx
+++ frontend/src/components/Discovery.jsx
@@ -4,12 +4,13 @@ import {connect} from "react-redux";
 import {discovery} from "../actions/index"
 
 class Discovery extends Component {
-	
+
 	componentDidMount() {
 		this.props.listIndustries();
 	}
 
 	render() {
+		console.log("PROPS", this.props);
 		return (
 			<div>
 				{this.props.allIndustry.map(industry => (
