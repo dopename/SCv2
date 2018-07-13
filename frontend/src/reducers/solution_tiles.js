e2705763aece189da2e5beb158a09d034ddf49c4
nmarini <marini.nick@gmail.com>
2018-07-13 10:50:27 -0700

    Solution tiles setup

--- frontend/src/reducers/solution_tiles.js
+++ frontend/src/reducers/solution_tiles.js
@@ -8,7 +8,8 @@ const initialState = {
 export default function solution_tiles(state = initialState, action) {
 	switch (action.type) {
 		case "FETCH_SOLUTION_DATA":
-			return {...state, solutionData: action.data}
+			console.log(action.data);
+			return {...state, solutionData: [...state.solutionData, ...action.data]}
 		default:
 			return state;
 		}
