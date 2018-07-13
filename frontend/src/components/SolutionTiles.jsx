7a41ea2fcfa99963cb2fd5ea2978b9480b17db84
nmarini <marini.nick@gmail.com>
2018-07-13 13:23:54 -0700

    updating soluton serializer

--- frontend/src/components/SolutionTiles.jsx
+++ frontend/src/components/SolutionTiles.jsx
@@ -33,8 +33,8 @@ class SolutionTiles extends Component {
 			    			<hr className="my-2" />
 			    			<div className="solution-title text-left">
 			    				<h3 className="text-center">{solution.name}</h3>
-			    				<p className="ml-2">{solution.provider_name}</p>
-			    				<p className="ml-2"><i className={solution.status === "Available Now" ? "text-success fa fa-check-circle-o" : "text-warning fa fa-clock-o"}>{solution.status}: {solution.status_date}</i></p>
+			    				<p className="ml-2 mb-1">{solution.provider_name}</p>
+			    				<p className="ml-2"><i className={solution.status === "Available since" ? "text-success fa fa-check-circle-o" : "text-warning fa fa-clock-o"}>{solution.status}: {solution.status_date}</i></p>
 			    			</div>
 			    		</div>
 		    		</div>)
