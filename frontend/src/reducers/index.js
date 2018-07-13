74182f39ccd35ca1f993d9244d623a52328a2cfe
nmarini <marini.nick@gmail.com>
2018-07-13 14:36:37 -0700

    updating soluton serializer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import discovery from "./discovery";
 import solution_tiles from "./solution_tiles";
+import main from "./main";
 
 const scApp = combineReducers({
 	discovery,
 	solution_tiles,
+	main
 })
 
 export default scApp;
