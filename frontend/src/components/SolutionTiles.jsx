c1f0c9759ef9ed608f3c8d3432675c27c23220e3
nmarini <marini.nick@gmail.com>
2018-07-13 11:00:41 -0700

    Solution tiles setup

--- frontend/src/components/SolutionTiles.jsx
+++ frontend/src/components/SolutionTiles.jsx
@@ -19,6 +19,15 @@ class SolutionTiles extends Component {
 		console.log("SOLUTION TILES PROPS", this.props)
 		return (
 			<div>
+				{this.props.solutionData.map(solution => {
+					if (this.props.unselected.indexOf(solution.category[0]) < 0) {
+						return (
+							<div className="col-lg-3 col-md-6">
+								<h3>{solution.name}</h3>
+							</div>
+						)
+					}
+				})}
 			</div>
 		)
 	}
@@ -27,6 +36,7 @@ class SolutionTiles extends Component {
 const mapStateToProps = state => {
 	return {
 		solutions:state.discovery.solutions,
+		unselected:state.discovery.unselectedCategories,
 		solutionData:state.solution_tiles.solutionData,
 
 	}
