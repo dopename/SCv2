9b8659ad24641646db8c0fa125befdc33f4894ba
nmarini <marini.nick@gmail.com>
2018-07-13 10:42:42 -0700

    Solution tiles setup

--- frontend/src/actions/solution_tiles.js
+++ frontend/src/actions/solution_tiles.js
@@ -3,6 +3,7 @@ import * as helpers from "../helpers/index";
 export const fetchSolutionData = (solutions) => {
 	return dispatch => {
 		let queries = helpers.fetchAPICall("solution", solutions)
+		return queries
 
 		Promise.all(queries).then(data => {
 			return dispatch({
