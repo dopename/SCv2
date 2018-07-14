633308ba6ab43eeaff6f41269b09c509275cde8c
unknown <marini.nick@gmail.com>
2018-07-13 22:53:57 -0700

    touching up css

--- frontend/src/components/dumb_components/SolutionModal.jsx
+++ frontend/src/components/dumb_components/SolutionModal.jsx
@@ -3,73 +3,78 @@ import { Modal, ModalBody, ModalFooter, Button } from "reactstrap";
 import Tags from "./Tags"
 
 class SolutionModal extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.state = {
-      modal:false,
-      mobile:null
-    }
+		this.state = {
+			modal:false,
+			mobile:null
+		}
 
-    this.checkIfActive = this.checkIfActive.bind(this);
-    //this.updateSolutionViews = this.updateSolutionViews.bind(this);
-  }
+		this.checkIfActive = this.checkIfActive.bind(this);
+		//this.updateSolutionViews = this.updateSolutionViews.bind(this);
+	}
 
-  componentDidMount() {
-    this.checkIfActive();
-  }
+	componentDidMount() {
+		this.checkIfActive();
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps) {
-      this.checkIfActive();
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (this.props !== prevProps) {
+			this.checkIfActive();
+		}
+	}
 
-  // updateSolutionViews() {
-  //     var data = this.props.solution
-  //     data.views += 1
-  //     var url = 'https://www.solutionconnect.org/api/postings/solution/update/'
-  //     fetch(url + this.props.solution.pk + '/', {
-  //       method: 'put',
-  //       headers: {
-  //         'content-type':'application/json',
-  //       },
-  //       body: JSON.stringify(data)
-  //     })
-  // }
+	// updateSolutionViews() {
+	//     var data = this.props.solution
+	//     data.views += 1
+	//     var url = 'https://www.solutionconnect.org/api/postings/solution/update/'
+	//     fetch(url + this.props.solution.pk + '/', {
+	//       method: 'put',
+	//       headers: {
+	//         'content-type':'application/json',
+	//       },
+	//       body: JSON.stringify(data)
+	//     })
+	// }
 
-  checkIfActive() {
-    var isMobile = this.props.screen_width < 540 ? true : false;
-    if (this.props.activeModal === this.props.solution.pk) {
-      this.setState({modal:true, mobile:isMobile});
-      // this.updateSolutionViews();
-    }
-    else {
-      this.setState({modal:false});
-    }
-  }
+	checkIfActive() {
+		var isMobile = this.props.screen_width < 540 ? true : false;
+		if (this.props.activeModal === this.props.solution.pk) {
+			this.setState({modal:true, mobile:isMobile});
+			// this.updateSolutionViews();
+		}
+		else {
+			this.setState({modal:false});
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <Modal className="col-12" isOpen={this.state.modal} toggle={this.props.toggle}>
-          <h4 className="mb-2"><i className="fa fa-window-close mx-1 float-right pointer-hand" onClick={() => {  this.props.toggle() }}></i><i className="fa fa-bookmark mx-1 float-right pointer-hand"></i><i className="fa fa-share-alt-square mx-1 float-right pointer-hand"></i></h4>
-          <div className="text-center">
-            <h2>{this.props.solution.name} by {this.props.solution.provider_name}</h2>
-            <p className="alert alert-secondary">{this.props.solution.status} {this.props.solution.status_date}</p>
-          </div>
-          <ModalBody>
-            <br/>
-            <h4 class="m-0">Who it's for:</h4>
-            <Tags tags={this.props.solution.tags} />
-          </ModalBody>
-          <ModalFooter>
-            <Button outline color="primary" className="btn-block">Additional Solution Media</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Modal size={this.state.mobile ? "md":"lg"} isOpen={this.state.modal} toggle={this.props.toggle}>
+					<h4 className="mb-2"><i className="fa fa-window-close mx-1 float-right pointer-hand" onClick={() => {  this.props.toggle() }}></i><i className="fa fa-bookmark mx-1 float-right pointer-hand"></i><i className="fa fa-share-alt-square mx-1 float-right pointer-hand"></i></h4>
+					<div className="row">
+						<div className="col-10 text-left">
+							<h2>{this.props.solution.name}</h2>
+							<p>{this.props.solution.provider_name}</p>
+						</div>
+						<div className="col-2">
+							<p className="ml-2 mb-1"><i className={this.props.solution.status === "Available since" ? "text-success fa fa-check-circle-o" : "text-warning fa fa-clock-o"}></i> {this.props.solution.status}: {this.props.solution.status_date}</p>
+						</div>
+					</div>
+					<ModalBody>
+						<br/>
+						<h4 class="m-0">Who it's for:</h4>
+						<Tags tags={this.props.solution.tags} />
+					</ModalBody>
+					<ModalFooter>
+						<Button outline color="primary" className="btn-block">Additional Solution Media</Button>
+					</ModalFooter>
+				</Modal>
+			</div>
+		)
+	}
 }
 
 export default SolutionModal;
