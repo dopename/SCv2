cb671d233e6cebfe916937e5b0edf9de17765e06
unknown <marini.nick@gmail.com>
2018-07-13 17:41:17 -0700

    Adding in state reset

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -56,26 +56,26 @@ class Initial extends Component {
 						<div className="col-lg-6 h-100 text-center pt-3" style={{backgroundColor:"#e6e6e6"}}>
 							<h2>To your profile</h2>
 							<div className="row">
-								<div className="col-md-3">
+								<div className="col-md-2">
 								</div>
-								<div className="col-md-6">
+								<div className="col-md-8">
 									<p className="px-3">Customize what content you subscribe to and stay up to date on the latest innovations.
 									Your profile is a one-stop shop for innovative tech so you can maximize your efficiency.</p>
 								</div>
-								<div className="col-md-3">
+								<div className="col-md-2">
 								</div>
 							</div>
 						</div>
 						<div className="col-lg-6 h-100 text-center pt-3" style={{borderLeft:"solid black 1px", backgroundColor:"#eef4fc"}}>
 							<h2>To the Discovery Environment</h2>
 							<div className="row">
-								<div className="col-md-3">
+								<div className="col-md-2">
 								</div>
-								<div className="col-md-6">
+								<div className="col-md-8">
 									<p className="px-3">Know you need something, but don't quite know what you're looking for?
 									Head over to the discovery environment and see new technology in every industry imaginable</p>
 								</div>
-								<div className="col-md-3">
+								<div className="col-md-2">
 								</div>
 							</div>
 							<Button outline color="secondary" size="lg"><Link to="/discovery">Go Discover!</Link></Button>
