e7c595bb6e786a2820bd6d8671395a87882ec15c
unknown <marini.nick@gmail.com>
2018-07-13 21:50:58 -0700

    touching up css

--- frontend/src/components/Discovery.jsx
+++ frontend/src/components/Discovery.jsx
@@ -75,8 +75,8 @@ class CheckBoxItem extends Component {
 	render() {
 		return (
 			<div className="input-group">
-				<div className="input-group-prepend mb-1" style={{vertialAlign:"center"}}>
-					<input className="mb-1" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
+				<div className="input-group-prepend">
+					<input className="mt-2" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
 				</div>
 				<h5 className={"mb-1" + this.props.type === "industry" ? " font-weight-bold" : ""}>{this.props.item.name}</h5>
 			</div>
