897e4b950b8bfe1c75eb64070b8e9326dfa20fe7
unknown <marini.nick@gmail.com>
2018-07-13 21:51:58 -0700

    touching up css

--- frontend/src/components/Discovery.jsx
+++ frontend/src/components/Discovery.jsx
@@ -76,7 +76,7 @@ class CheckBoxItem extends Component {
 		return (
 			<div className="input-group">
 				<div className="input-group-prepend">
-					<input className="mt-2" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
+					<input className="mt-1" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
 				</div>
 				<h5 className={"mb-1" + this.props.type === "industry" ? " font-weight-bold" : ""}>{this.props.item.name}</h5>
 			</div>
