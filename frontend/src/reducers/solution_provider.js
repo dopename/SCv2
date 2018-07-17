c7901aa09d0ffe096d6f953bcc2653baa15073b9
nmarini <marini.nick@gmail.com>
2018-07-17 12:18:48 -0700

    Adding in SolutionProvider Redux

--- frontend/src/reducers/solution_provider.js
+++ frontend/src/reducers/solution_provider.js
@@ -8,7 +8,7 @@ export default function solution_provider(state = initialState, action) {
 	switch (action.type) {
 		case "FETCH_PROVIDER":
 			console.log(action.provider);
-			return {...state, providerData: provider}
+			return {...state, providerData: action.provider}
 		default:
 			return state;
 		}
