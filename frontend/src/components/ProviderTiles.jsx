9534baf64bc1e0736e0e2702c8f4d218231905ba
nmarini <marini.nick@gmail.com>
2018-07-18 10:09:37 -0700

    Abstracting tiles

--- frontend/src/components/ProviderTiles.jsx
+++ frontend/src/components/ProviderTiles.jsx
@@ -20,6 +20,7 @@ class ProviderTiles extends Component {
 			let queries = fetchAPICall(this.props.solutions);
 
 			Promise.all(queries).then(returnData => {
+				console.log("DID MOUNT",returnData);
 				this.setState({
 					solutions:returnData
 				})
@@ -32,6 +33,7 @@ class ProviderTiles extends Component {
 			let queries = fetchAPICall(this.props.solutions);
 
 			Promise.all(queries).then(returnData => {
+				console.log("DID UPDATE",returnData);
 				this.setState({
 					solutions:returnData
 				})
