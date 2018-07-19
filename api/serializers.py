858e60d937016e2509939e070adf0b6776a1d3ff
nmarini <marini.nick@gmail.com>
2018-07-19 11:20:38 -0700

    Trying to patch up User return API

--- api/serializers.py
+++ api/serializers.py
@@ -40,7 +40,7 @@ class UserLoginSerializer(serializers.ModelSerializer):
 		fields = ['username', 'pk']
 
 class CustomUserSerializer(serializers.ModelSerializer):
-	#user = UserSerializer(read_only=True)
+	user = UserSerializer(read_only=True)
 	seeker_account = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 	provider_acount = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
@@ -48,9 +48,9 @@ class CustomUserSerializer(serializers.ModelSerializer):
 		model = CustomUser
 		fields = [
 			'pk',
-			#'user',
-			'seeker_user',
-			'provider_user'
+			'user',
+			'seeker_account',
+			'provider_account'
 		]
 
 #/------------------------------------------------------------------------------/
