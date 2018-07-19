1532e4ddd6001d8c8e72b471ea2237752caa0016
nmarini <marini.nick@gmail.com>
2018-07-19 16:04:36 -0700

    Working on the background image..

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -109,33 +109,20 @@ class Initial extends Component {
 							</div>
 						</div>
 					</div>
-					<div className="row h-50" style={{borderTop:"solid black 1px"}}>
-						<div className="col-lg-6 h-50 text-center pt-3" style={{backgroundColor:"#e6e6e6"}}>
-							<h2>To your profile</h2>
-							<div className="row">
-								<div className="col-md-2">
-								</div>
-								<div className="col-md-8">
-									<p className="px-3">Customize what content you subscribe to and stay up to date on the latest innovations.
-									Your profile is a one-stop shop for innovative tech so you can maximize your efficiency.</p>
-								</div>
-								<div className="col-md-2">
-								</div>
+					<div className="col-12">
+						<h1 className="text-center">Connecting businesses with solutions to help them succeed</h1>
+					</div>
+					<div className="row h-75" style={{borderTop:"solid black 1px"}}>
+						<div className="col-lg-8 h-100 text-left">
+							<div className="col-12">
+								<h2>Technological innovation is accelerating at an exponential rate, and it can be difficult to keep up with the developments that matter to you.</h2>
+							</div>
+							<div className="col-12 mt-2">
+								<h2>Solution Connect demystifies cutting edge technologies to help you find and connect with the solutions you need.</h2>
 							</div>
 						</div>
-						<div className="col-lg-6 h-50 text-center pt-3" style={{borderLeft:"solid black 1px", backgroundColor:"#eef4fc"}}>
-							<h2>To the Discovery Environment</h2>
-							<div className="row">
-								<div className="col-md-2">
-								</div>
-								<div className="col-md-8">
-									<p className="px-3">Know you need something, but don't quite know what you're looking for?
-									Head over to the discovery environment and see new technology in every industry imaginable</p>
-								</div>
-								<div className="col-md-2">
-								</div>
-							</div>
-							<Button outline color="secondary" size="lg"><Link to="/discovery">Go Discover!</Link></Button>
+						<div className="col-lg-4 h-100">
+							<img style={{backgroundPosition:"center"}} src="/media/home-2.png" alt="Solution man" />
 						</div>
 					</div>
 				</div>
