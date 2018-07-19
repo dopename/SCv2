372fdf3fa0ac99ba897c0780dda630f0b568491a
nmarini <marini.nick@gmail.com>
2018-07-19 10:27:18 -0700

    Trying to patch up User return API

--- api/views.py
+++ api/views.py
@@ -45,7 +45,7 @@ class RegistrationAPI(generics.GenericAPIView):
 #/--------------------------------------------------------------/        
 
 class UserAPI(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticated, ]
+    permission_classes = (permissions.AllowAny, )
     serializer_class = CustomUserSerializer
 
     def get_object(self):
