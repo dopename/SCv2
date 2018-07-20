b4e6f628c2c1c890f599c6978c9ca20d13c15c9c
unknown <marini.nick@gmail.com>
2018-07-19 21:34:52 -0700

    adding in seeker profile stuff

--- frontend/src/actions/seeker_account.js
+++ frontend/src/actions/seeker_account.js
@@ -4,7 +4,7 @@ export const retrieveSeekerAccount = (seekerAccountPK) => {
 	return dispatch => {
 		return helpers.retrieveAPICall("seekeraccount", seekerAccountPK)
 			.then(res => res.json())
-			.then(seeker => {
+			.then(seekerAccountData => {
 				return dispatch({
 					type: "FETCH_SEEKER_ACCOUNT",
 					seekerAccountData
