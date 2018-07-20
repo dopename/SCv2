f8d20e3bef570f8731e9750c8d70b4c0e525e606
nmarini <marini.nick@gmail.com>
2018-07-20 09:07:29 -0700

    fizing typo

--- frontend/src/components/SeekerProfile.jsx
+++ frontend/src/components/SeekerProfile.jsx
@@ -14,7 +14,7 @@ class SeekerProfile extends Component {
 
 	componentDidUpdate(prevProps) {
 		if (this.props != prevProps) {
-			if (this.props.auth.user != null) {
+			if (this.props.auth.user != null && !this.props.isLoaded) {
 				this.props.retrieveSeekerAccount(this.props.auth.user.custom_user.seeker_account)
 			}
 		}
