0f178fb862f838dd0d0c426b8085f14da2cecb17
unknown <marini.nick@gmail.com>
2018-07-22 12:50:47 -0700

    Dropdown menu on nav bar

--- frontend/src/components/SeekerSettings.jsx
+++ frontend/src/components/SeekerSettings.jsx
@@ -88,7 +88,7 @@ class SeekerSettings extends Component {
 
 		this.props.industries.map((i, index) => {
 			cols.push(
-				<div className="col-4">
+				<div className="col-4 text-center">
 					<DumbCheckBox item={i} checked={i.categories.map(c => c.pk).some(r => this.state.unselectedCategories.includes(r)) ? false : true} checkBox={this.checkBox} type="industry" />
 					<DumbSubCheckBox items={i.categories} unselected={this.state.unselectedCategories} checkBox={this.checkBox} />
 				</div>
