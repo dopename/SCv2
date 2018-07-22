819c34e2442cdb756ea6e518bcdaf2b9aee76971
unknown <marini.nick@gmail.com>
2018-07-22 12:59:56 -0700

    Dropdown menu on nav bar

--- frontend/src/components/dumb_components/DumbCheckBox.jsx
+++ frontend/src/components/dumb_components/DumbCheckBox.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class DumbCheckBox extends Component {
 	render() {
 		return (
-			<div className={"input-group" + (this.props.center ? " center" : "")}>
+			<div className={"input-group" + (this.props.center ? " text-center" : "")}>
 				<div className="input-group-prepend">
 					<input className="mt-1" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
 				</div>
@@ -16,7 +16,7 @@ export class DumbCheckBox extends Component {
 export class DumbSubCheckBox extends Component {
 	render() {
 		return (
-			<ul className={"mb-0" + (this.props.center ? " center" : "")}>
+			<ul className={"mb-0" + (this.props.center ? " text-center" : "")}>
 				{this.props.items.map(e => (
 					<li><DumbCheckBox item={e} checked={this.props.unselected.indexOf(e.pk) > -1 ? false : true} checkBox={this.props.checkBox} type="category" /></li>
 				))}
