5e96b3c7a132c24807c0610384744bf8b7f63184
nmarini <marini.nick@gmail.com>
2018-07-25 12:44:28 -0700

    Adding in mobile responsive containers for homep age

--- frontend/src/components/dumb_components/DumbCheckBox.jsx
+++ frontend/src/components/dumb_components/DumbCheckBox.jsx
@@ -7,7 +7,7 @@ export class DumbCheckBox extends Component {
 				<div className="input-group-prepend">
 					<input className="mt-1" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
 				</div>
-				<p className={"mb-1 pl-1" + this.props.type === "industry" ? " font-weight-bold" : ""}>{this.props.item.name}</p>
+				<p className={"mb-0 ml-1" + (this.props.type === "industry" ? " font-weight-bold" : "")}>{this.props.item.name}</p>
 			</div>
 		)
 	}
