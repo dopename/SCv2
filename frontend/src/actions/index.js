e030b074c7b59e8ae4487664c14497ef5875b4cb
unknown <marini.nick@gmail.com>
2018-07-25 18:26:21 -0700

    Adding in provider profile

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -4,6 +4,6 @@ import * as main from "./main";
 import * as solution_provider from "./solution_provider";
 import * as auth from "./auth";
 import * as seeker_account from "./seeker_account";
-import * as provider_acocunt from "./provider_account";
+import * as provider_account from "./provider_account";
 
 export {discovery, solution_tiles, main, solution_provider, auth, seeker_account, provider_account}
