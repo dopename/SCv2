c3a6759479b68a7d2cd6aa55507d6235ea469b97
nmarini <marini.nick@gmail.com>
2018-07-27 15:50:05 -0700

    Adding overfloy for d-env

--- frontend/src/components/DiscoveryTiles.jsx
+++ frontend/src/components/DiscoveryTiles.jsx
@@ -18,7 +18,7 @@ class DiscoveryTiles extends Component {
 	}
 
 	render() {
-		document.body.classList.add("noscroll");
+		//document.body.classList.add("noscroll");
 		var renderSolutions = [];
 
 		this.props.solutionData.map(solution => {
