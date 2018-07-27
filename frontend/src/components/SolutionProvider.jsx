e34090d2da05ddfa13814d47c443a9a2deff3d04
unknown <marini.nick@gmail.com>
2018-07-26 17:48:42 -0700

    Positioning stuff for provider page

--- frontend/src/components/SolutionProvider.jsx
+++ frontend/src/components/SolutionProvider.jsx
@@ -27,14 +27,20 @@ class SolutionProvider extends Component {
 							<div className="col-lg-2">
 								<img className="responsive-image m-auto" style={{maxHeight:"200px"}} alt={this.props.providerData.name + " logo"} src={this.props.providerData.logo} />
 							</div>
-							<div className="col-lg-6 border-bottom mb-2">
-								<h1 className="text-center">{this.props.providerData.name}</h1>
-								<p className="text-center"><i>{this.props.providerData.tagline}</i></p>
+							<div className="col-lg-8 border-bottom mb-2">
+								<h1 className="text-left text-primary">{this.props.providerData.name}</h1>
+								<p className="text-left"><i>{this.props.providerData.tagline}</i></p>
 							</div>
-							<div className="col-lg-4">
+							<div className="col-lg-2">
 								<p>www.marinibank.com</p>
 								<p>Social links to go here</p>
 							</div>
+						</div>
+						<hr className="mb-2" style={{borderBottom:"dotted black 1px"}} />
+						<div className="col-12 border-bottom">
+							<h4 className="d-inline">Overview</h4>
+							<h4 className="d-inline">Solutions</h4>
+							<h4 className="d-inline">About</h4>
 						</div>
 						<div className="row mb-2 pb-2 border-bottom">
 							<div className="col-lg-6">
@@ -51,8 +57,8 @@ class SolutionProvider extends Component {
 								<p>{this.props.providerData.about_us}</p>
 							</div>
 						</div>
-						<div id="solutions">
-							<h3 className="text-left">Solutions by {this.props.providerData.name}</h3>
+						<div id="solutions" style={{borderTop:"dotted black 1px"}}>
+							<h3 className="text-left mt-2 text-primary">Solutions by {this.props.providerData.name}</h3>
 							{this.props.providerData ? <ProviderTiles solutions={this.props.providerData.solutions} /> : <div className="loader"></div>}
 						</div>
 					</div>
