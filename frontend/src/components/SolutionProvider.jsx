7e26dded8974abc51d339e2880f710812686a60f
unknown <marini.nick@gmail.com>
2018-07-26 18:22:11 -0700

    Positioning stuff for provider page

--- frontend/src/components/SolutionProvider.jsx
+++ frontend/src/components/SolutionProvider.jsx
@@ -39,7 +39,7 @@ class SolutionProvider extends Component {
 									</tbody>
 								</table>
 							</div>
-							<div className="col-lg-2 d-flex flex-column align-items-end">
+							<div className="col-lg-2 d-flex align-items-end">
 								<div className="p-2">Social links to go here</div>
 								<div className="p-2">www.marinibank.com</div>
 							</div>
