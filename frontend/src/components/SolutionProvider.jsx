1d3b4b2ee2bfe04b5ce7e0867ed13165c8fccd6f
nmarini <marini.nick@gmail.com>
2018-08-02 15:40:03 -0700

    padding, border, etc. for provider page

--- frontend/src/components/SolutionProvider.jsx
+++ frontend/src/components/SolutionProvider.jsx
@@ -19,7 +19,7 @@ class SolutionProvider extends Component {
 		console.log(this.props);
 
 		return (
-			<div className="container-fluid mt-4">
+			<div className="container-fluid mt-5">
 				<div className="row">
 					<div className="col-lg-1 d-none d-sm-block">
 					</div>
@@ -49,20 +49,20 @@ class SolutionProvider extends Component {
 						</div>
 						<hr className="mb-2" style={{borderBottom:"dotted black 1px"}} />
 						<div className="col-12 border-bottom">
-							<h5 className="d-inline mx-2 text-primary">Overview</h5>
-							<h5 className="d-inline mx-2 text-primary">Solutions</h5>
-							<h5 className="d-inline mx-2 text-primary">About</h5>
+							<p className="d-inline mx-2 text-primary"><strong>Overview</strong></p>
+							<p className="d-inline mx-2 text-primary"><strong>Solutions</strong></p>
+							<p className="d-inline mx-2 text-primary"><strong>About</strong></p>
 						</div>
 						<div className="row my-4 pt-2">
 							<div className="col-lg-6">
 								<div className="provider-tags p-2">
-									<h4 className="text-left">We make solutions for:</h4>
+									<p className="text-left"><strong>We make solutions for:</strong></p>
 									{this.props.providerData ? <Tags tags={this.props.providerData.child_tags} /> : <div className="loader"></div>}
 								</div>
 							</div>
 							<div className="col-lg-6">
 								<div className="provider-tags p-2">
-									<h4 className="text-left">Industries we're in:</h4>
+									<p className="text-left"><strong>Industries we're in:</strong></p>
 									{this.props.providerData ? <Tags tags={this.props.providerData.child_ind_cat} /> : <div className="loader"></div>}
 								</div>
 							</div>
