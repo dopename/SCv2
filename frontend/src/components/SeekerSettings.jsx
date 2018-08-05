454bfe29a1c2432245c87a57cbb1db895a509f92
unknown <marini.nick@gmail.com>
2018-08-04 23:02:05 -0700

    main back in

--- frontend/src/components/SeekerSettings.jsx
+++ frontend/src/components/SeekerSettings.jsx
@@ -104,7 +104,7 @@ class SeekerSettings extends Component {
 
 		this.props.industries.map((i, index) => {
 			cols.push(
-				<div className="col-md-3">
+				<div className="col-md-4">
 					<DumbCheckBox item={i} checked={i.categories.map(c => c.pk).some(r => this.state.unselectedCategories.includes(r)) ? false : true} center={true} checkBox={this.checkBox} type="industry" />
 					<DumbSubCheckBox items={i.categories} unselected={this.state.unselectedCategories} checkBox={this.checkBox} center={true} />
 				</div>
@@ -121,7 +121,7 @@ class SeekerSettings extends Component {
 		 if (this.props.tags) {
 		 	this.props.tags.map(t => {
 		 		tagCol.push(
-		 			<div className="col-md-3">
+		 			<div className="col-md-4">
 		 				<DumbCheckBox item={t} checked={this.state.selectedTags.indexOf(t.pk) < 0 ? false : true} checkBox={this.tagCheck} center={true} />
 		 			</div>
 		 			)
