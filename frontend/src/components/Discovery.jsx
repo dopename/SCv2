a0bc15afdaa8de66740f7ce5fc5334f9a2305da8
nmarini <marini.nick@gmail.com>
2018-08-15 14:02:33 -0700

    Mobile functionality for discovery environment

--- frontend/src/components/Discovery.jsx
+++ frontend/src/components/Discovery.jsx
@@ -6,11 +6,26 @@ import {discovery} from "../actions/index"
 import DiscoveryTiles from "./DiscoveryTiles"
 import "./Discovery.css"
 import { DumbCheckBox, DumbSubCheckBox } from "./dumb_components/DumbCheckBox";
+import {Button, Modal} from "reactstrap";
 
 class Discovery extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			modalOpen:false
+		}
+
+		this.toggleModal = this.toggleModal.bind(this);
+	}
+
 
 	componentDidMount() {
 		this.props.listIndustries();
+	}
+
+	toggleModal() {
+		this.setState({modalOpen:!this.state.modalOpen})
 	}
 
 	render() {
@@ -19,23 +34,18 @@ class Discovery extends Component {
 		return (
 			<div className="container-fluid h-100">
 				<div className="row h-100">
-					<div className="col-lg-3 text-center discovery-sidebar border border-info">
-						<h3 className="mt-2"><i className="fa fa-search"></i> Refine Results</h3>
-						<input className="form-control mb-2" type="text" placeholder="What would you like to find?" />
-						<div className="text-left discovery-area">
-							<div className="text-center discovery-header">
-								<h4>Solution Categories</h4>
-							</div>
-							<div className="discovery-body p-1">
-								{this.props.discovery.industries.map(i => (
-									<div>
-										<DumbCheckBox item={i} checked={i.categories.map(c => c.pk).some(r => this.props.discovery.unselectedCategories.includes(r)) ? false : true} checkBox={this.props.checkBox} type="industry" />
-										<DumbSubCheckBox items={i.categories} unselected={this.props.discovery.unselectedCategories} checkBox={this.props.checkBox} />
-									</div>
-								))}
-							</div>
-						</div>
-					</div>
+					{!this.props.mobile.isMobile ? 
+						(
+							<RefineResults discovery={this.props.discovery} checkBox={this.props.checkBox} />
+						)
+						:
+						(
+							<Button outline color="info" size="md" onClick={() => this.toggleModal()}>Refine Results</Button>
+							<Modal isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+								<RefineResults discovery={this.props.discovery} checkBox={this.props.checkBox} />
+							</Modal>
+						)
+					}
 					<div className="col-lg-9">
 						<div className="row">
 							<div className="col-12">
@@ -49,6 +59,7 @@ class Discovery extends Component {
 		)
 	}
 }
+
 
 const mapStateToProps = state => {
 	return {
@@ -74,27 +85,26 @@ const mapDispatchToProps = dispatch => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(Discovery);
 
-// class CheckBoxItem extends Component {
-// 	render() {
-// 		return (
-// 			<div className="input-group">
-// 				<div className="input-group-prepend">
-// 					<input className="mt-1" type="checkbox" onClick={() => this.props.checkBox(this.props.item.pk, this.props.type)} checked={this.props.checked} />
-// 				</div>
-// 				<h5 className={"mb-1" + this.props.type === "industry" ? " font-weight-bold" : ""}>{this.props.item.name}</h5>
-// 			</div>
-// 		)
-// 	}
-// }
-
-// class SubCheckBoxItems extends Component {
-// 	render() {
-// 		return (
-// 			<ul className="mb-0">
-// 				{this.props.items.map(e => (
-// 					<li><CheckBoxItem item={e} checked={this.props.unselected.indexOf(e.pk) > -1 ? false : true} checkBox={this.props.checkBox} type="category" /></li>
-// 				))}
-// 			</ul>
-// 		)
-// 	}
-// }
+class RefineResults extends Component {
+	render() {
+		return (
+			<div className="col-lg-3 text-center discovery-sidebar border border-info">
+				<h3 className="mt-2"><i className="fa fa-search"></i> Refine Results</h3>
+				<input className="form-control mb-2" type="text" placeholder="What would you like to find?" />
+				<div className="text-left discovery-area">
+					<div className="text-center discovery-header">
+						<h4>Solution Categories</h4>
+					</div>
+					<div className="discovery-body p-1">
+						{this.props.discovery.industries.map(i => (
+							<div>
+								<DumbCheckBox item={i} checked={i.categories.map(c => c.pk).some(r => this.props.discovery.unselectedCategories.includes(r)) ? false : true} checkBox={this.props.checkBox} type="industry" />
+								<DumbSubCheckBox items={i.categories} unselected={this.props.discovery.unselectedCategories} checkBox={this.props.checkBox} />
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
