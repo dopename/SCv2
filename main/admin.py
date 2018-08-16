4d78e9c040b686fab82fc8535464316e3d205ba0
nmarini <marini.nick@gmail.com>
2018-08-16 11:59:31 -0700

    return medialocation better name

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from main.models import CustomUser, Industry, Category, MediaLocation, Media, SolutionMedia,\
 	TagType, Tag, Provider, ProviderAccount, SeekerAccount, Solution, TeamMember
 
+class SolutionMediaAdmin(admin.ModelAdmin):
+	list_display = ('solutionmedia_id', 'solution')
+
+	def solution(self, instance):
+		return instance.solution.name
+
 admin.site.register(CustomUser)
 admin.site.register(Industry)
 admin.site.register(Category)
 admin.site.register(MediaLocation)
 admin.site.register(Media)
-admin.site.register(SolutionMedia)
+admin.site.register(SolutionMedia, SolutionMediaAdmin)
 admin.site.register(TagType)
 admin.site.register(Tag)
 admin.site.register(Provider)
