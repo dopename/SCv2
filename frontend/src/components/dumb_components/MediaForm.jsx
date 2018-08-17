55287fbbb93093f958922840b2f71fc93f612a9f
unknown <marini.nick@gmail.com>
2018-08-16 22:43:11 -0700

    Adding location name to Media Serializer

--- frontend/src/components/dumb_components/MediaForm.jsx
+++ frontend/src/components/dumb_components/MediaForm.jsx
@@ -74,6 +74,7 @@ export default class MediaForm extends Component {
 		e.preventDefault();
 
 		var data = {...this.state}
+		data.solutionmedia = data.solutionmedia.toInt()
 
 		delete data.allLocations;
 
