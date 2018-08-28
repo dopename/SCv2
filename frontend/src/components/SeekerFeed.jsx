8de2d59a3a921a232d049f5dc9afb788a0b02f77
nmarini <marini.nick@gmail.com>
2018-08-28 11:15:26 -0700

    Touching up CSS for new feed

--- frontend/src/components/SeekerFeed.jsx
+++ frontend/src/components/SeekerFeed.jsx
@@ -11,12 +11,12 @@ export default class SeekerFeed extends Component {
 		var responsiveHeight = (0.18 * this.props.mobile.screen_height).toString() + "px"
 		this.props.solutions.map(s => {
 			feed.push(
-					<div className="col-12">
-						<div className="d-flex flex-column">
+					<div className="col-12 my-1">
+						<div className="d-flex flex-column border border-dark" style={{borderRadius:"10px"}}>
 							<div className="d-flex flex-row">
 								<img src={s.main_image} alt="pic" style={{maxHeight:responsiveHeight}} className="responsive-image mt-2" />
 							</div>
-							<p className="text-left p-2">{s.about}</p>
+							<p className="text-left p-2">{s.about_us}</p>
 							<Tags tags={s.tags} />
 						</div>
 					</div>
