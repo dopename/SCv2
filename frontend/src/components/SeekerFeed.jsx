e5fec5aeb4e36007a142a2bfe1131139bc39f587
nmarini <marini.nick@gmail.com>
2018-08-28 11:40:00 -0700

    Exerimental image responsive thing"

--- frontend/src/components/SeekerFeed.jsx
+++ frontend/src/components/SeekerFeed.jsx
@@ -6,26 +6,70 @@ import Tags from "./dumb_components/Tags";
 
 //mobile
 export default class SeekerFeed extends Component {
+	constructor(props) {
+		super(props) 
+
+		this.state = {
+			leftView:[],
+			topView:[]
+		}
+	}
+
+	checkDimensions(img, pk) {
+
+	}
+
 	render() {
 		var feed = [];
 		var responsiveHeight = (0.18 * this.props.mobile.screen_height).toString() + "px"
 		this.props.solutions.map(s => {
-			feed.push(
-					<div className="col-12 my-1">
-						<div className="d-flex flex-column border border-dark" style={{borderRadius:"10px"}}>
-							<div className="d-flex flex-row">
-								<img src={s.main_image} alt="pic" style={{maxHeight:responsiveHeight}} className="responsive-image mt-2" />
-							</div>
-							<p className="text-left p-2">{s.why}</p>
-							<Tags tags={s.tags} />
-						</div>
-					</div>
-				)
+			var feedImage = <img src={s.main_image} alt="pic" style={{maxHeight:responsiveHeight}} className="responsive-image mt-2" />
+			if (feedImage.naturalWidth / feedImage.naturalHeight > 1.70) {
+				feed.push(<TopFeedItem s={s} image={feedImage} />)
+			}
+			else {
+				feed.push(<LeftFeedItem s={s} image={feedImage} />)
+			}
+
 		})
 
 		return ( 
 			<div className="d-flex flex-column">
 				{feed}
+			</div>
+		)
+	}
+}
+
+class LeftFeedItem extends Component {
+
+	render() {
+		return (
+			<div className="col-12 my-1">
+				<div className="d-flex flex-column border border-dark" style={{borderRadius:"10px"}}>
+					<div className="d-flex flex-row">
+						{this.props.image}
+						<p className="text-left p-2">{s.why}</p>
+					</div>
+					<Tags tags={s.tags} />
+				</div>
+			</div>
+		)
+	}
+}
+
+class TopFeedItem extends Component {
+
+	render() {
+		return (
+			<div className="col-12 my-1">
+				<div className="d-flex flex-column border border-dark" style={{borderRadius:"10px"}}>
+					<div className="d-flex flex-row">
+						{this.props.image}
+					</div>
+					<p className="text-left p-2">{s.why}</p>
+					<Tags tags={s.tags} />
+				</div>
 			</div>
 		)
 	}
